7514589f142d4e7a5ac91f2df7c9e144d94a298f
Xin Meng <mengxin.city@gmail.com>
2014-11-11 13:43:23 +0000

    Nov. 11 Finish DataProcess_V2 Add DataProcess_V3, backup before fix the V3 file

--- DataProcess_v3.py
+++ DataProcess_v3.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 # ############################################
-##Sample data process V2.0
+##Sample data process V3.0
 ##Author:Xin Meng
-##Data:  2014 Oct 29th
-##Description:
+##Data:  2014 Nov 11th
+##Description: Process the packet file and combinate the packet with same stream index into one stream
+##             And generate the dataset based on the stream.
+##
 #############################################
 
 
